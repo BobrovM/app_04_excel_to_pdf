78921057d80b0e794aacb07e2802f609d6d5c100
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-08 16:46:21 +0100

    Course day 26. Tables in main project. Finished the student project.

--- main.py
+++ main.py
@@ -13,10 +13,45 @@ for filepath in filepaths:
     pdf.add_page()
 
     filename = Path(filepath).stem
-    invoice_nr = "Invoice nr. " + filename.split("-")[0]
 
+    invoice_nr, date = filename.split("-")
+    invoice_nr = "Invoice nr. " + invoice_nr
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=0, h=12, txt=invoice_nr, ln=12, align="L")
+    pdf.cell(w=0, h=8, txt=invoice_nr, ln=1, align="L")
+
+    date = "Date: " + date
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=0, h=8, txt=date, ln=1, align="L")
+
+    columns = list(df.columns)
+    columns_header = [item.replace("_", " ").title() for item in columns]
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_fill_color(200, 200, 200)
+
+    pdf.cell(w=30, h=6, txt=columns_header[0], fill=True, border=1)
+    pdf.cell(w=73, h=6, txt=columns_header[1], fill=True, border=1)
+    pdf.cell(w=32, h=6, txt=columns_header[2], fill=True, border=1)
+    pdf.cell(w=30, h=6, txt=columns_header[3], fill=True, border=1)
+    pdf.cell(w=30, h=6, txt=columns_header[4], fill=True, border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.cell(w=30, h=6, txt=str(row[columns[0]]), border=1)
+        pdf.cell(w=73, h=6, txt=row[columns[1]], border=1)
+        pdf.cell(w=32, h=6, txt=str(row[columns[2]]), border=1)
+        pdf.cell(w=30, h=6, txt=str(row[columns[3]]), border=1)
+        pdf.cell(w=30, h=6, txt=str(row[columns[4]]), border=1, ln=1)
+
+    total_sum = str(df[columns[4]].sum())
+    pdf.cell(w=30, h=6, txt="", border=1)
+    pdf.cell(w=73, h=6, txt="", border=1)
+    pdf.cell(w=32, h=6, txt="", border=1)
+    pdf.cell(w=30, h=6, txt="", border=1)
+    pdf.cell(w=30, h=6, txt=total_sum, fill=True, border=1, ln=1)
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(w=45, h=12, txt="RandomCompanyName")
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"pdfs/{filename}.pdf")
-
-
--- student_project/sp.py
+++ student_project/sp.py
@@ -16,4 +16,14 @@ for filepath in filepaths:
     pdf.set_font("Times", "B", 16)
     pdf.cell(20, 10, txt=filename, ln=16)
 
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    #file = open(filepath)
+    #content = file.read()
+    #file.close()
+
+    pdf.set_font("Times", size=12)
+    pdf.multi_cell(w=0, h=8, txt=content)
+
 pdf.output("output.pdf")
